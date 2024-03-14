fc4b7f160210427a9065585ed7100f12acbc0257
tarunrajput <tarunrajput1337@gmail.com>
2024-03-15 02:13:55 +0530

    feat: add reddit integration

---
+++ integrations/reddit/setup.ts
@@ -0,0 +1,16 @@
+import { beforeAll, afterAll } from "bun:test";
+import { resource } from "./resource.ts";
+
+// any sdk setup here if needed
+
+// load environment variables if needed (that aren't in resource.ts)
+
+beforeAll(() => {
+  // setup code here
+  console.log('BEFOREALL: Setup process');
+})
+
+afterAll(() => {
+  // cleanup code here
+  console.log('AFTERALL: Cleanup process');
+})
