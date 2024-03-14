c3d43246703eb29d835e1f6607cba5c0b540d0bd
HugoCasa <hugo@casademont.ch>
2024-03-14 13:15:48 +0100

    fix: improve example

--- integrations/github/Create_branch/script.bun.test.ts
+++ integrations/github/Create_branch/script.bun.test.ts
@@ -1,56 +1,48 @@
-import { expect, test } from "bun:test";
-import { main } from "./script.bun.ts";
-import { resource } from "../resource.ts";
-import { Octokit } from "octokit";
+import { expect, test } from 'bun:test'
+import { main } from './script.bun.ts'
+import { resource } from '../resource.ts'
+import { Octokit } from 'octokit'
 
-test("Create branch", async () => {
-  // script arguments
-  const owner = Bun.env.OWNER!;
-  const repo = Bun.env.REPO!;
-  const sourceBranchName = "main";
-  const newBranchName = "feature/new-branch";
+test('Create branch', async () => {
+	// script arguments
+	const owner = Bun.env.OWNER!
+	const repo = Bun.env.REPO!
+	const sourceBranchName = 'main'
+	const newBranchName = 'feature/new-branch'
 
-  console.log(
-    `TEST: Will test creating a new branch on the repository ${owner}/${repo}`
-  );
+	console.log(`TEST: Will test creating a new branch on the repository ${owner}/${repo}`)
 
-  // check if branch exists, delete it if it does
-  const octokit = new Octokit({
-    auth: resource.token,
-  });
+	// check if branch exists, delete it if it does
+	const octokit = new Octokit({
+		auth: resource.token
+	})
 
-  try {
-    const { data: newBranch } = await octokit.rest.repos.getBranch({
-      branch: newBranchName,
-      repo,
-      owner,
-    });
+	try {
+		const { data: newBranch } = await octokit.rest.repos.getBranch({
+			branch: newBranchName,
+			repo,
+			owner
+		})
 
-    await octokit.rest.git.deleteRef({
-      owner,
-      repo,
-      ref: `refs/heads/${newBranchName}`,
-      sha: newBranch.commit.sha,
-    });
-  } catch (e) {}
+		await octokit.rest.git.deleteRef({
+			owner,
+			repo,
+			ref: `refs/heads/${newBranchName}`,
+			sha: newBranch.commit.sha
+		})
+	} catch (e) {}
 
-  // calling main
-  console.log("TEST: Running main function");
-  const response = await main(
-    resource,
-    owner,
-    repo,
-    sourceBranchName,
-    newBranchName
-  );
+	// calling main
+	console.log('TEST: Running main function')
+	const response = await main(resource, owner, repo, sourceBranchName, newBranchName)
 
-  // assertions here
-  expect(response).toBe("Branch feature/new-branch created");
-  // check if branch exists
-  const { data: newBranch } = await octokit.rest.repos.getBranch({
-    branch: newBranchName,
-    repo,
-    owner,
-  });
-  expect(newBranch.name).toBe(newBranchName);
-});
+	// assertions here
+	expect(response.ref).toBe(`refs/heads/${newBranchName}`)
+	// check if branch exists
+	const { data: newBranch } = await octokit.rest.repos.getBranch({
+		branch: newBranchName,
+		repo,
+		owner
+	})
+	expect(newBranch.name).toBe(newBranchName)
+})
--- integrations/github/Create_branch/script.bun.ts
+++ integrations/github/Create_branch/script.bun.ts
@@ -1,36 +1,36 @@
-import { Octokit } from "octokit";
+import { Octokit } from 'octokit'
 
 type Github = {
-  token: string;
-};
+	token: string
+}
 
 export async function main(
-  resource: Github,
-  owner: string,
-  repo: string,
-  sourceBranchName: string,
-  newBranchName: string
+	resource: Github,
+	owner: string,
+	repo: string,
+	sourceBranchName: string,
+	newBranchName: string
 ) {
-  // setup auth
-  const octokit = new Octokit({
-    auth: resource.token,
-  });
+	// setup auth
+	const octokit = new Octokit({
+		auth: resource.token
+	})
 
-  // get sha of source branch
-  const { data: sourceBranch } = await octokit.rest.repos.getBranch({
-    branch: sourceBranchName,
-    repo,
-    owner,
-  });
-  const sha = sourceBranch.commit.sha;
+	// get sha of source branch
+	const { data: sourceBranch } = await octokit.rest.repos.getBranch({
+		branch: sourceBranchName,
+		repo,
+		owner
+	})
+	const sha = sourceBranch.commit.sha
 
-  // create new branch
-  await octokit.rest.git.createRef({
-    owner,
-    repo,
-    ref: `refs/heads/${newBranchName}`,
-    sha,
-  });
+	// create new branch
+	const response = await octokit.rest.git.createRef({
+		owner,
+		repo,
+		ref: `refs/heads/${newBranchName}`,
+		sha
+	})
 
-  return `Branch ${newBranchName} created`;
+	return response.data
 }
