54304ce4a7e87c49b1e2af66f4b711a1816962a6
neo773 <62795688+neo773@users.noreply.github.com>
2024-03-15 23:39:49 +0530

    add changes

---
+++ integrations/zoom/List_Channels/script.bun.ts
@@ -0,0 +1,24 @@
+import zoomApi, { type ListChannelMembersResponse } from 'zoomapi';
+import type { resource } from '../resource';
+
+export type ListChannelMembersParams = {
+    channel_id: string;
+    page_size?: number;
+    next_page_token?: string;
+};
+
+export async function main(resource: resource, channelParams: ListChannelMembersParams) {
+    const client = zoomApi(resource);
+    let nextPageToken: string | undefined;
+    let allMembers: ListChannelMembersResponse['members'] = [];
+
+    do {
+        const params = nextPageToken ? { next_page_token: nextPageToken } : {};
+        const response = await client.chat.ListChannelMembers({ ...channelParams, ...params });
+        allMembers = allMembers.concat(response.members);
+        nextPageToken = response.next_page_token;
+    } while (nextPageToken);
+
+    return { members: allMembers };
+}
+
